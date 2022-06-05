9a6f8f98dacdd905e1768aecf60146a19b9c6830
Hank <hank2272sr@gmail.com>
2022-06-05 12:32:44 +0800

    add final-project

--- second semester/final-project/final_project.py
+++ second semester/final-project/final_project.py
@@ -1,7 +1,7 @@
-from turtle import position
-from pycat.core import Window, Sprite, RotationMode, KeyCode, Point
+from random import random
+from pycat.core import Window, Sprite, RotationMode, KeyCode, Point, Scheduler
 from enum import Enum, auto
-
+from random import randint
 
 
 w = Window(is_sharp_pixel_scaling=True)
@@ -12,13 +12,21 @@ SCALE_MIN = 45
 SCALE_MAX = 60
 S_D = SCALE_MAX - SCALE_MIN
 D = 50
-
+ATK_DMG = 5
 def get_scale(y):
     a = (Y_MAX - y)/(Y_MAX - Y_MIN)
     return SCALE_MIN+a*S_D
 
 
+def spwan_enemy(dt):
+    w.create_sprite(Enemy)
+
+
+Scheduler.update(spwan_enemy, 2)
+
+
 class Enemy(Sprite):
+    total_enemies = 0
 
     class State(Enum):
         WAIT = auto()
@@ -26,6 +34,9 @@ class Enemy(Sprite):
         DIE = auto()
 
     def on_create(self):
+        Enemy.total_enemies += 1
+        if Enemy.total_enemies > 3:
+            Scheduler.cancel_update(spwan_enemy)
         self.add_tag('enemy')
         self.y = 0
         self.rotation_mode = RotationMode.RIGHT_LEFT
@@ -35,11 +46,33 @@ class Enemy(Sprite):
         self.y = w.height/2
         self.image = 'img/Wait.PNG'
         self.state = Enemy.State.WAIT
+        self.healthbar = w.create_sprite(Healthbar)
+        self.hp = 100
+        self.is_attacking = False
+        self.attacktime = 0
+        self.goto_random_position()
+
+    def delete():
+        super().delete()
+        Enemy.total_enemies -= 1
+        if Enemy.total_enemies < 3:
+            Scheduler.update(spwan_enemy, 2)
 
 
     def on_update(self, dt):
+        if self.is_attacking:
+            self.attacktime += dt
+            if self.attacktime >= 2:
+                self.attacktime = 0
+                self.is_attacking = False
+        elif self.is_touching_sprite(player):
+            self.is_attacking = True
+            player.hp-= ATK_DMG
+            player.healthbar.set_width(player.hp/100)
         self.scale = get_scale(self.y)*0.02
         self.time += dt
+        self.healthbar.position = self.position
+        self.healthbar.y += self.height/2+5
         if self.time >= 1.5:
             self.state = Enemy.State.CHASE
         if self.state == Enemy.State.CHASE:
@@ -47,6 +80,12 @@ class Enemy(Sprite):
             self.move_forward(D)
             self.state = Enemy.State.WAIT
             self.time = 0
+        
+        sword = self.get_touching_sprites_with_tag('attack')
+        if sword:
+            self.hp -= ATK_DMG
+            self.healthbar.set_width(self.hp/100)
+            sword[0].remove_tag("attack")
         
         
         
@@ -61,22 +100,36 @@ class Enemy(Sprite):
         pass
 
 
-class E_healthbar(Sprite):
+class Healthbar(Sprite):
     def on_create(self):
         self.scale_x = 100
+    
+    def set_width(self, v: float):
+        self.scale_x = 100*v
 
-    def on_update(self, dt):
-        self.position = enemy.position
-        self.y = enemy.y + 40
+
+    # def on_update(self, dt):
+    #     self.position = enemy.position
+    #     self.y = enemy.y + 40
 
 class Player(Sprite):
+    class State(Enum):
+        WAIT = auto()
+        BE_ATK = auto()
+        DIE = auto()
+
     def on_create(self):
+        self.state = self.State.WAIT
         self.rotation_mode = RotationMode.RIGHT_LEFT
         self.scale = 100
-        self.x = w.width/2
+        self.x = 0
         self.y = w.height/2
         self.move_dir = Point(0,0)
+        self.healthbar = w.create_sprite(Healthbar)
+        self.hp = 100
     def on_update(self, dt):
+        self.healthbar.position = self.position
+        self.healthbar.y += self.height/2+5
         self.scale = get_scale(self.y)
         self.point_toward_sprite(enemy)
         self.move_dir = Point(0,0)
@@ -93,22 +146,27 @@ class Player(Sprite):
         else:
             self.move_dir *= 1
         self.position += self.move_dir
-        if w.is_key_pressed(KeyCode.N):
+        if w.is_key_down(KeyCode.N):
             w.create_sprite(Sword)
+        
+        
 
 
 class Sword(Sprite):
     def on_create(self):
+        self.rotation = -30
         self.scale = 0.5
         self.position = player.position
+        self.x += player.width/2 + 10
         self.image = 'img/sword.png'
+        self.add_tag('attack')
     def on_update(self, dt):
-        self.rotation -= 2
-        if self.rotation <= -30:
+        self.rotation -= 5
+        if self.rotation <= -120:
             self.delete()
 
 
 enemy = w.create_sprite(Enemy)
 player = w.create_sprite(Player)
-e_healthbar = w.create_sprite(E_healthbar)
+
 w.run()
